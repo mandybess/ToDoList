20e09f7ccc7ebe6ce636aabaab6bcfb42e142dd9
Rachael Hobbs <hobbs.rachael@gmail.com>
2016-03-05 15:24:20 -0500

    More Firebase setup

---
+++ app/src/main/java/com/rahobbs/todo/helpers/Constants.java
@@ -0,0 +1,13 @@
+package com.rahobbs.todo.helpers;
+
+import com.rahobbs.todo.BuildConfig;
+
+/**
+ * Created by rachael on 3/5/16.
+ */
+public class Constants {
+
+    //Firebase URL
+    public static final String FIREBASE_URL = BuildConfig.UNIQUE_FIREBASE_ROOT_URL;
+
+}
